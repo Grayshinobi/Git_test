59ff370ec3e767b501c672e1d7177da9976886c4
Greyshinobi2013 <greyshinobi2013@gmail.com>
2021-09-02 11:57:26 +0300

    Dice roller app

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    lateinit var diceImage : ImageView
+    lateinit var diceNumber:TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.roll_button)
+        Toast.makeText(this, "coded by GreyShinobi" , Toast.LENGTH_LONG).show()
+        rollButton.setOnClickListener{
+            rollDice()
+        }
+        diceImage = findViewById(R.id.dice_image)
+        diceNumber = findViewById(R.id.dice_number)
+    }
+
+    private fun  rollDice(){
+
+        val randomInt = Random().nextInt(6)+1
+        val drawableResource = when (randomInt){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+        diceNumber.text = randomInt.toString()
+    }
+}
