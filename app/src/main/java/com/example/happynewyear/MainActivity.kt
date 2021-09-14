48bf38442b7015b3fb3418d21560d0b0d2b8de14
Greyshinobi2013 <greyshinobi2013@gmail.com>
2021-09-14 10:41:48 +0300

    2nd App

--- app/src/main/java/com/example/happynewyear/MainActivity.kt
+++ app/src/main/java/com/example/happynewyear/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.example.happynewyear
 
-import android.app.Activity
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.happynewyear.databinding.ActivityMainBinding
 
@@ -15,18 +13,19 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, "Developed by Natnael.G", Toast.LENGTH_SHORT).show();
-        setContentView(R.layout.activity_main)
 
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this, "Coded by Greyshinobi2013", Toast.LENGTH_LONG).show();
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.Done.setOnClickListener {
-           addContent(it)
+            addContent(it)
         }
 
 
     }
-    private fun addContent(view: View){
+
+    private fun addContent(view: View) {
         val name = binding.FistInput
         var textInput = binding.wish.text
 
@@ -35,12 +34,12 @@ class MainActivity : AppCompatActivity() {
         binding.wish.text = "To ${name.text} and  your family!!!"
 
 
-        binding.welcome.visibility=View.GONE
-        binding.FistInput.visibility= View.GONE
-        binding.Done.visibility= View.GONE
-        binding.Gif.visibility=View.VISIBLE
-        binding.wish.visibility= View.VISIBLE
-        binding.Gif2.visibility=View.VISIBLE
+        binding.welcome.visibility = View.GONE
+        binding.FistInput.visibility = View.GONE
+        binding.Done.visibility = View.GONE
+        binding.Gif.visibility = View.VISIBLE
+        binding.wish.visibility = View.VISIBLE
+        binding.Gif2.visibility = View.VISIBLE
 
         // Hide the keyboard.
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
@@ -48,4 +47,4 @@ class MainActivity : AppCompatActivity() {
 
 
     }
-    }
+}
